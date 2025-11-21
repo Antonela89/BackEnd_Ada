e3279f14cce3f07b21d07225ebdc560b45a24c22
Antonela Borgogno <antonelaborgogno@gmail.com>
2025-11-21 12:05:42 -0300

    teorico

---
+++ modulo_3/modulo.3-ASINCRONIA/clase-11/teorico/3.trycatch.js
@@ -0,0 +1,33 @@
+// Simulamos una funcion que solicita datos, con una opcion de exito o error
+function obtenerDatos(exito) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (exito) {
+                resolve(`Datos recibidos correctamente.`)  // resuleve con exito
+            } else {
+                reject(`Error al obtener los datos.`) // Lanza un error
+            }
+        }, 3000);
+    });
+}
+
+// Funcion asincrona que intenta obtener los datos y maneja posiblres errores
+async function consultarDatos() {
+    try {
+        console.log("Iniciando consulta de datos...");
+
+        const exito = Math.random() > 0.5
+        // Pausa hasta que obtenerDatos() se complete o atrapa un error si ocurre
+        let resultado = await obtenerDatos(exito);
+        console.log(resultado); // Imprime si fue exitoso
+        
+    } catch (error) {
+        console.error("Ocurrio un error: ", error);  // Captura y muestra el error
+    }
+    finally {
+        console.log("Finalizando consulta de datos."); // Siempre se ejecuta
+    }
+}
+
+
+consultarDatos();
