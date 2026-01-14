0b9c668e6f0ea01b21fa25adc6d69768a47e154d
Antonela Borgogno <antonelaborgogno@gmail.com>
2026-01-13 21:16:55 -0300

    controlador

---
+++ modulo_3/modulo.5-REPASO/clase-21/teorico/biblioteca/backend/controllers/bookController.js
@@ -0,0 +1,70 @@
+// Importación del modelo
+import { readBooks, writeBooks } from '../models/bookModel.js';
+
+const BookController = () => {
+    // GET
+    const getAllBooks = (req, res) => {
+        // Llamamos al modelo
+        res.json(readBooks());
+    }
+
+    const getById = (req, res) => {
+        const books = readBooks();
+
+        const book = books.find(b => b.id === req.params.id);
+
+        if (!book) {
+            return res.status(404).json({message: `Libro: ${req.params.id} no encontrado.`})
+        }
+
+        res.json(book)
+    }   
+
+    // POST
+    const createBook = (req, res) => {
+        const {title, author, genre} = req.body;
+        const books = readBooks();
+
+        const newBook = {
+            id: Date.now(),
+            title,
+            author,
+            genre
+        }
+
+        books.push(newBook);
+
+        writeBooks(books);
+
+        res.status(201).json({message: 'Libro creado con éxito.'},  newBook)
+    }
+
+    //PATCH
+    const updateBook = (req, res) => {
+        const books = readBooks();
+
+        const index = books.findIndex(b => b.id === req.params.id);
+
+        if (index = -1 ) {
+            res.status(404).json({error: 'Libro no encontrado'})
+        }
+
+        books[index] = {...books[index],  ...req.body};
+
+        writeBooks(books);
+
+        res.status(200).json({message: 'Libro actualizado'}, books[index])
+    }
+
+    //DELETE
+    const deleteBook = (req, res) => {
+        const books = readBooks();
+
+        const filteredBoks = books.filter(b = b.id !== req.params.id);
+
+        writeBooks(filteredBoks);
+        res.status(200).json({message: `Libro ${req.params.id} borrado con éxito.`})
+    }
+}
+
+export default BookController;
