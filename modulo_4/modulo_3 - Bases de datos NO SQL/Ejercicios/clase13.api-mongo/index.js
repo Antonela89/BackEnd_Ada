b176c770aa6e1efbfc4ed88ce102cc12de732c4d
Antonela Borgogno <antonelaborgogno@gmail.com>
2026-04-02 20:31:50 -0300

    index.js para configuración del servidor y rutas de usuarios

---
+++ modulo_4/modulo_3 - Bases de datos NO SQL/Ejercicios/clase13.api-mongo/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { connectDB } from './connectDB.js';
+
+dotenv.config();
+
+// Importar rutas
+const userRoutes = await import('./routes/userRoutes.js');
+
+// Esperar a que la conexión a la base de datos se establezca
+await connectDB(); 
+
+// Configuración del servidor
+// Crear una instancia de Express
+const app = express();
+// Puerto de escucha
+const PORT = process.env.PORT || 3000; 
+
+// Middleware
+// Permitir solicitudes CORS y parsear JSON
+app.use(cors());
+// Middleware para parsear JSON en las solicitudes
+app.use(express.json());
+
+// Rutas
+// Ruta de ejemplo para verificar que el servidor está funcionando
+app.get('/', (req, res) => {
+    res.json({ message: '¡Servidor API en funcionamiento!' });
+});
+
+// Usar las rutas de usuarios
+app.use('/api/users', userRoutes);
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
