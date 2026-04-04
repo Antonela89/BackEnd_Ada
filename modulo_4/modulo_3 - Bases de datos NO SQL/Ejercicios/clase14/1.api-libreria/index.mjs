6f85eeda4e8811ddb973ba8249b394fb7a859ce1
Antonela Borgogno <antonelaborgogno@gmail.com>
2026-04-04 20:42:27 -0300

    se agrego ruta al app.listen

--- modulo_4/modulo_3 - Bases de datos NO SQL/Ejercicios/clase14/1.api-libreria/index.mjs
+++ modulo_4/modulo_3 - Bases de datos NO SQL/Ejercicios/clase14/1.api-libreria/index.mjs
@@ -17,5 +17,5 @@ app.use('/api/libros', LibrosRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-	console.log(`Servidor corriendo en el puerto ${PORT}`);
+	console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
 });
