204ea96b7d8b72cffd58bc8df68df2441803c389
Antonela Borgogno <antonelaborgogno@gmail.com>
2026-04-04 21:09:21 -0300

    logica eliminacion en cascada

--- modulo_4/modulo_3 - Bases de datos NO SQL/Ejercicios/clase14/2.api-veterinaria/models/Cliente.mjs
+++ modulo_4/modulo_3 - Bases de datos NO SQL/Ejercicios/clase14/2.api-veterinaria/models/Cliente.mjs
@@ -1,9 +1,20 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import Mascota from './Mascota.mjs';
 
 const clienteSchema = new mongoose.Schema({
-    nombre: { type: String, required: true },
-    telefono: { type: String },
-    direccion: { type: String }
+	nombre: { type: String, required: true },
+	telefono: { type: String },
+	direccion: { type: String },
+});
+
+clienteSchema.pre('findOneAndDelete', async function (next) {
+	try {
+		const clienteId = this.getQuery()['_id'];
+		await Mascota.deleteMany({ duenio: clienteId });
+		next();
+	} catch (error) {
+		next(error);
+	}
 });
 
 const Cliente = mongoose.model('Cliente', clienteSchema);
