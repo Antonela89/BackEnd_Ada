a438814d59034cdcc721bd5d296a68bb0d0f5c23
Antonela Borgogno <antonelaborgogno@gmail.com>
2026-04-07 21:14:32 -0300

    front

--- modulo_4/modulo_4 - Ciberseguiridad/clase15/1.Restaurante/index.mjs
+++ modulo_4/modulo_4 - Ciberseguiridad/clase15/1.Restaurante/index.mjs
@@ -6,9 +6,16 @@ import connectDB from './database.mjs';
 import PlatosRouter from './routes/Platos.mjs';
 import PedidosRouter from './routes/Pedidos.mjs';
 import ClientesRouter from './routes/Clientes.mjs';
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+import serverless from 'serverless-http';
 
 dotenv.config()
 connectDB();
+
+// --- RECREAR __dirname ---
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -17,7 +24,15 @@ app.use(helmet()); // Protección de cabeceras
 app.use(cors());
 app.use(express.json()); // Permitir recibir JSON en el body
 
-// Rutas (API REST)
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// --- RUTA DE BIENVENIDA (Landing Page de Ciberseguridad) ---
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Rutas Modulares (API REST)
 app.use('/api/platos', PlatosRouter);
 app.use('/api/pedidos', PedidosRouter);
 app.use('/api/clientes', ClientesRouter);
@@ -27,3 +42,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+export const handler = serverless(app);
